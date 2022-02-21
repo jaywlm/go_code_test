24d8e97f676ca7e25d7922e3f820bec3a8cf21b6
jaywlm <732144170@qq.com>
2022-02-21 15:45:57 +0800

    Create t6.go

---
+++ t6.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hello world")
+	w.(http.Flusher).Flush()
+	time.Sleep(time.Second)
+	fmt.Fprintln(w, "hello world")
+	w.(http.Flusher).Flush()
+}
+func IndexHandler2(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hello world2")
+	w.(http.Flusher).Flush()
+	time.Sleep(time.Second)
+	fmt.Fprintln(w, "hello world2")
+	fmt.Fprintln(w, r.URL.Path, r.URL.RawQuery)
+	fmt.Fprintln(w, r.URL)
+	w.(http.Flusher).Flush()
+}
+
+func main() {
+	http.HandleFunc("/a/", IndexHandler)
+	http.HandleFunc("/aa/", IndexHandler2)
+	http.Li
